c7d0fa3e28df10629c9562be171b813c4ca5c0cc
Polova <booozyby@gmail.com>
2020-07-15 23:35:12 +0300

    login

--- screens/login/LoginScreen.js
+++ screens/login/LoginScreen.js
@@ -1,14 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 
 
 export default class LoginScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text> Hello World! </Text>
+        <View style={{height: '50%', width: '100%', backgroundColor: '#002B2B', alignItems: 'center', justifyContent: 'center'}}>
+          <Image resizeMode='contain' style={{width: '30%', height: '30%'}} source={require('../../content/images/sapoAppLogo.png')}/>
+        </View>
+        <View style={{height: '50%', width: '100%', backgroundColor: 'white'}}>
+          {this.getEmailPasswordForm()}
+        </View>
       </View>
     );
+  }
+
+  getEmailPasswordForm(){
+    return(
+      <View style={{alignItems: 'center', justifyContent: 'center', flexDirection: 'column', marginTop: '10%'}}>
+        <View style={{width: '70%', backgroundColor: '#EFEFEF', borderRadius: 12, paddingTop: 6, paddingBottom: 0, paddingHorizontal: 15, marginHorizontal: 5, }}>
+          <TextInput
+            keyboardType={Platform.OS === 'android' ? 'email-address' : 'ascii-capable'} 
+            onChangeText={(text) => {}}
+            style={{borderColor: '#002B2B', textAlign: 'center', borderBottomWidth: 1, fontSize: 16, marginBottom: 7, paddingBottom: 2}}  placeholder="Email" />
+        </View>
+        <View style={{width: '70%', backgroundColor: '#EFEFEF', marginTop: 10, marginBottom: 5, borderRadius: 12, paddingTop: 6, paddingBottom: 0, paddingHorizontal: 15, marginHorizontal: 5, }}>
+          <TextInput
+            onChangeText={(text) => {}}
+            secureTextEntry={true}
+            autoCapitalize = 'none' 
+            style={{borderColor: '#002B2B', textAlign: 'center', borderBottomWidth: 1, fontSize: 16, marginBottom: 7, paddingBottom: 2}} placeholder="Пароль" /> 
+        </View>
+        <View style={{margin: 5, width: '70%', marginTop: 10}}>
+          <TouchableOpacity
+            onPress={() => {}}
+            style={{backgroundColor: "#002B2B", alignItems: 'center', justifyContent: 'center', height: 35, borderRadius: 12}}>
+              <Text style={{color: 'white', fontSize: 15}}>Увійти</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{alignItems: 'center', margin: 5, }}>
+            <Text>Авторизуйтеся через E-mail</Text>
+        </View>
+    </View>);
   }
 }
 
@@ -16,6 +50,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        flexDirection: 'column'
     }
   });
