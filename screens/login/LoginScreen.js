5d3c225b95c5f8f53c298c148eaaef8a8b715b91
Polova <booozyby@gmail.com>
2020-07-16 00:26:02 +0300

    login tabs

--- screens/login/LoginScreen.js
+++ screens/login/LoginScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
-
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, Dimensions } from 'react-native';
+import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
 
 export default class LoginScreen extends React.Component {
   render() {
@@ -10,10 +10,18 @@ export default class LoginScreen extends React.Component {
           <Image resizeMode='contain' style={{width: '30%', height: '30%'}} source={require('../../content/images/sapoAppLogo.png')}/>
         </View>
         <View style={{height: '50%', width: '100%', backgroundColor: 'white'}}>
-          {this.getEmailPasswordForm()}
+          {this.getTabView()}
         </View>
       </View>
     );
+  }
+
+  getTabView(){     
+    const SecondRoute = () => (
+      <View style={[styles.scene, { backgroundColor: '#673ab7' }]} />
+    );
+
+    return(<LoginTabView firstRoute={this.getEmailPasswordForm} secondRoute={SecondRoute}/>)
   }
 
   getEmailPasswordForm(){
@@ -46,11 +54,48 @@ export default class LoginScreen extends React.Component {
   }
 }
 
+function LoginTabView(props) {
+  const initialLayout = { width: Dimensions.get('window').width };
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'first', title: 'За email'},
+    { key: 'second', title: 'За адресою' },
+  ]);
+ 
+  const renderScene = SceneMap({
+    first: props.firstRoute,
+    second: props.secondRoute,
+  });
+ 
+  return (
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={initialLayout}
+      renderTabBar={(props) => {
+        return (
+          <TabBar
+            {...props}
+            activeColor="#002B2B"
+            inactiveColor="#c2c3c8"
+            indicatorStyle={{ backgroundColor: '#002B2B' }}
+            style={{ backgroundColor: '#fff' }}
+          />
+        )
+      }}
+    />
+  );
+}
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
         flexDirection: 'column'
+    },
+    scene: {
+        flex: 1,
     }
   });
