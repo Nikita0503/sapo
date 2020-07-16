3f7847474659b150e5d99289c64a38c9a68aa3d3
Polova <booozyby@gmail.com>
2020-07-16 15:47:47 +0300

    login desing tabs

--- screens/login/LoginScreen.js
+++ screens/login/LoginScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, Dimensions, ScrollView } from 'react-native';
 import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
+import SearchableDropdown from 'react-native-searchable-dropdown';
 
 export default class LoginScreen extends React.Component {
   render() {
@@ -21,7 +22,7 @@ export default class LoginScreen extends React.Component {
       <View style={[styles.scene, { backgroundColor: '#673ab7' }]} />
     );
 
-    return(<LoginTabView firstRoute={this.getEmailPasswordForm} secondRoute={this.getAddressFrom}/>)
+    return(<LoginTabView firstRoute={this.getEmailPasswordForm} secondRoute={this.getAddressForm}/>)
   }
 
   getEmailPasswordForm(){
@@ -53,28 +54,254 @@ export default class LoginScreen extends React.Component {
     </View>);
   }
 
-  getAddressFrom(){
+  getAddressForm(){
+    var items = [
+      {id: 1, name: 'Київська',},
+      {id: 2, name: 'Дніпровська',},
+      { id: 3, name: 'Харківська',},
+      { id: 4, name: 'Запорізька',},
+      { id: 5, name: 'Вінницька',},
+      { id: 6, name: 'Волинська',},
+      { id: 7, name: 'Рівненська',},
+      { id: 8, name: 'Сумська',},
+    ];
+    var items2 = [
+      {id: 1, name: 'Київ',},
+      {id: 2, name: 'Дніпро',},
+      { id: 3, name: 'Харків',},
+      { id: 4, name: 'Кривий Ріг',},
+      { id: 5, name: 'Вінниця',},
+      { id: 6, name: 'Львів',},
+      { id: 7, name: 'Чернівці',},
+      { id: 8, name: 'Умань',},
+    ];
+    var items3 = [
+      { id: 1, name: 'Ватутіна',},
+      { id: 2, name: 'Кропівницького',},
+      { id: 3, name: 'Хрещатик',},
+      { id: 4, name: 'Криворіжсталі',},
+      { id: 5, name: 'Університет',},
+    ];
+    var items4 = [
+      { id: 1, name: '1А',},
+      { id: 2, name: '1Б',},
+      { id: 3, name: '2',},
+      { id: 4, name: '3',},
+      { id: 5, name: '4',},
+    ];
+    var items5 = [
+      { id: 1, name: '1',},
+      { id: 2, name: '3',},
+      { id: 3, name: '5',},
+      { id: 4, name: '6',},
+      { id: 5, name: '8',},
+    ];
     return(
+      <ScrollView>
       <View style={{alignItems: 'center'}}>
-        <View style={{width: '80%', backgroundColor: '#EFEFEF', borderRadius: 20, marginTop: 10}}>
-          <TouchableOpacity
-              onPress={() => {}}>
-            <Text style={{margin: 3, marginLeft: 15, color: 'gray'}}>Область</Text>
-            <Text style={{fontSize: 18, marginLeft: 15, marginBottom: 3}}>Київська</Text>
-            <View style={{width: '90%', borderBottomColor: 'gray', borderBottomWidth: 1, alignSelf: 'center', marginBottom: 10}}/>
-          </TouchableOpacity>
+        <View style={{alignItems: 'center', margin: 10 }}>
+            <Text>Авторизуйтеся за адресою</Text>
         </View>
-        <View style={{width: '80%', backgroundColor: '#EFEFEF', borderRadius: 20, marginTop: 10}}>
+        <View style={{width: '80%', backgroundColor: '#EFEFEF', borderRadius: 12, paddingVertical: 0}}>
+          <SearchableDropdown
+            onItemSelect={(item) => {
+              
+            }}
+            containerStyle={{ paddingHorizontal: 10, paddingVertical: 5 }}
+            itemStyle={{
+              padding: 10              
+            }}
+            itemsContainerStyle={{ maxHeight: 140, borderRadius: 12 }}
+            items={items}
+            textInputProps={
+              {
+                placeholder: "Область",
+                underlineColorAndroid: "transparent",
+                style: {
+                  width: '95%', 
+                  borderBottomWidth: 1, 
+                  borderColor: '#002B2B',
+                  alignSelf: 'center', 
+                  fontSize: 16,
+                  paddingTop: 0,
+                  paddingBottom: 1,
+                  marginBottom: 1
+                },
+                onTextChange: text => {}
+              }
+            }
+            listProps={
+              {
+                nestedScrollEnabled: true,
+              }
+            }
+          />
+        </View>
+
+        <View style={{width: '80%', backgroundColor: '#EFEFEF', borderRadius: 12, marginTop: 10, paddingVertical: 0}}>
+          <SearchableDropdown
+            onItemSelect={(item) => {
+              
+            }}
+            containerStyle={{ paddingHorizontal: 10, paddingVertical: 5 }}
+            itemStyle={{
+              padding: 10              
+            }}
+            itemsContainerStyle={{ maxHeight: 140, borderRadius: 12 }}
+            items={items2}
+            textInputProps={
+              {
+                placeholder: "Місто",
+                underlineColorAndroid: "transparent",
+                style: {
+                  width: '95%', 
+                  borderBottomWidth: 1, 
+                  borderColor: '#002B2B',
+                  alignSelf: 'center', 
+                  fontSize: 16,
+                  paddingTop: 0,
+                  paddingBottom: 1,
+                  marginBottom: 1
+                },
+                onTextChange: text => {}
+              }
+            }
+            listProps={
+              {
+                nestedScrollEnabled: true,
+              }
+            }
+          />
+        </View>
+
+        <View style={{width: '80%', backgroundColor: '#EFEFEF', borderRadius: 12, marginTop: 10, paddingVertical: 0}}>
+          <SearchableDropdown
+            onItemSelect={(item) => {
+              
+            }}
+            containerStyle={{ paddingHorizontal: 10, paddingVertical: 5 }}
+            itemStyle={{
+              padding: 10              
+            }}
+            itemsContainerStyle={{ maxHeight: 140, borderRadius: 12 }}
+            items={items3}
+            textInputProps={
+              {
+                placeholder: "Вулиця",
+                underlineColorAndroid: "transparent",
+                style: {
+                  width: '95%', 
+                  borderBottomWidth: 1, 
+                  borderColor: '#002B2B',
+                  alignSelf: 'center', 
+                  fontSize: 16,
+                  paddingTop: 0,
+                  paddingBottom: 1,
+                  marginBottom: 1
+                },
+                onTextChange: text => {}
+              }
+            }
+            listProps={
+              {
+                nestedScrollEnabled: true,
+              }
+            }
+          />
+        </View>
+
+        <View style={{width: '80%', backgroundColor: '#EFEFEF', borderRadius: 12, marginTop: 10, paddingVertical: 0}}>
+          <SearchableDropdown
+            onItemSelect={(item) => {
+              
+            }}
+            containerStyle={{ paddingHorizontal: 10, paddingVertical: 5 }}
+            itemStyle={{
+              padding: 10              
+            }}
+            itemsContainerStyle={{ maxHeight: 140, borderRadius: 12 }}
+            items={items4}
+            textInputProps={
+              {
+                placeholder: "Оберіть будинок",
+                underlineColorAndroid: "transparent",
+                style: {
+                  width: '95%', 
+                  borderBottomWidth: 1, 
+                  borderColor: '#002B2B',
+                  alignSelf: 'center', 
+                  fontSize: 16,
+                  paddingTop: 0,
+                  paddingBottom: 1,
+                  marginBottom: 1
+                },
+                onTextChange: text => {}
+              }
+            }
+            listProps={
+              {
+                nestedScrollEnabled: true,
+              }
+            }
+          />
+        </View>
+
+        <View style={{width: '80%', backgroundColor: '#EFEFEF', borderRadius: 12, marginTop: 10, paddingVertical: 0}}>
+          <SearchableDropdown
+            onItemSelect={(item) => {
+              
+            }}
+            containerStyle={{ paddingHorizontal: 10, paddingVertical: 5 }}
+            itemStyle={{
+              padding: 10              
+            }}
+            itemsContainerStyle={{ maxHeight: 140, borderRadius: 12 }}
+            items={items5}
+            textInputProps={
+              {
+                placeholder: "Оберіть квартиру",
+                underlineColorAndroid: "transparent",
+                style: {
+                  width: '95%', 
+                  borderBottomWidth: 1, 
+                  borderColor: '#002B2B',
+                  alignSelf: 'center', 
+                  fontSize: 16,
+                  paddingTop: 0,
+                  paddingBottom: 1,
+                  marginBottom: 1
+                },
+                onTextChange: text => {}
+              }
+            }
+            listProps={
+              {
+                nestedScrollEnabled: true,
+              }
+            }
+          />
+        </View>
+        <View style={{width: '80%', backgroundColor: '#EFEFEF', marginTop: 10, marginBottom: 5, borderRadius: 12, paddingTop: 6, paddingBottom: 0, paddingHorizontal: 15, marginHorizontal: 5, }}>
+          <TextInput
+            onChangeText={(text) => {}}
+            style={{borderColor: '#002B2B',  borderBottomWidth: 1, fontSize: 15, marginBottom: 7, paddingBottom: 2, paddingHorizontal: 2}} placeholder="Номер рахунку" /> 
+        </View>
+        <View style={{margin: 5, width: '80%', marginTop: 10, marginBottom: 40}}>
           <TouchableOpacity
-              onPress={() => {}}>
-            <Text style={{margin: 3, marginLeft: 15, color: 'gray'}}>Місто</Text>
-            <Text style={{fontSize: 18, marginLeft: 15, marginBottom: 3}}>Чернігів</Text>
-            <View style={{width: '90%', borderBottomColor: 'gray', borderBottomWidth: 1, alignSelf: 'center', marginBottom: 10}}/>
+            onPress={() => {}}
+            style={{backgroundColor: "#002B2B", alignItems: 'center', justifyContent: 'center', height: 35, borderRadius: 12}}>
+              <Text style={{color: 'white', fontSize: 15}}>Увійти</Text>
           </TouchableOpacity>
         </View>
       </View>
+      </ScrollView>
     )
   }
+
+  
+
+  
+  
 }
 
 function LoginTabView(props) {
